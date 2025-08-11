48863463d33abde9c6437c1300ada8c95d37363f
SambitParida <parida.sambit@gmail.com>
2025-08-11 13:27:56 -0400

    Create Date Dimension

--- nodeploy/execute_scripts.sql
+++ nodeploy/execute_scripts.sql
@@ -6,47 +6,31 @@ list @cricket_stg;
 SELECT * FROM raw.match_raw_tbl;
 
 create or replace transient table cricket.clean.match_detail_clean as
-select 
-    info:match_type_number::int as match_type_number,
-    info:event:name::text as event_name,
-    case 
-    when 
-        info:event:match_number::text is not null then info:event:match_number::text
-    when
-        info:event:stage::text is not null then info:event::text
-    else
-        'NA'
-    end as match_stage,
-    info:dates[0]::date as event_date,
-    date_part('year', info:dates[0]::date) as event_year,
-    date_part('month', info:dates[0]::date) as event_month,
-    date_part('day', info:dates[0]::date) as event_day,
-    info:match_type::text as match_type,
-    info:season::text as season,
-    info:team_type as team_type,
-    info:overs::text as overs,
-    info:city::text as city,
-    info:venue::text as venue, 
-    info:gender::text as gender,
-    info:teams[0]::text as first_team,
-    info:teams[1]::text as second_team,
-    case 
-        when info:outcome.winner is not null then 'Result Declared'
-        when info:outcome.result = 'tie' then 'Tie'
-        when info:outcome.result = 'no result' then 'No Result'
-        else info:outcome.result
-    end as matach_result,
-    case 
-        when info:outcome.winner is not null then info:outcome.winner
-        else 'NA'
-    end as winner,   
 
-    info:toss.winner::text as toss_winner,
-    initcap(info:toss.decision::text) as toss_decision,
-    --
-    stg_file_name ,
-    stg_file_row_number,
-    STG_FILE_CONTENT_KEY,
-    STG_MODIFIED_TS
+
+select 
+    raw.info:match_type_number::int as match_type_number,   
+    i.value:team::text as country,
+    o.value:over::text as overs,
+    d.value:bowler::text as bowler,
+    d.value:batter::text as batter,
+    d.value:non_striker::text as non_striker,
+    d.value:runs:batter::int as runs,
+    d.value:runs:extras::int as extras,
+    d.value:runs:total::int as total,
+    w.value:player_out::text as player_out,
+    w.value:kind::text as player_out_kind,
+    w.value:fielders::variant as fielders,
+    -- o.overs:over:value:over::int as over,
+    raw.stg_file_name,
+    raw.stg_file_row_number,
+    raw.STG_FILE_CONTENT_KEY,
+    raw.STG_MODIFIED_TS
 from
-cricket.raw.match_raw_tbl;
+cricket.raw.match_raw_tbl raw,
+LATERAL FLATTEN(input => raw.innings) i,
+LATERAL FLATTEN(input => i.value:overs) o,
+LATERAL FLATTEN(input => o.value:deliveries) d,
+LATERAL FLATTEN(input => d.value:wickets, outer => TRUE) w
+
+
